01ab39fa23156e7e7ad10d796b85e0ee7dbfe931
Pelayo28 <125690081+Pelayo28@users.noreply.github.com>
2023-06-08 15:49:45 +0200

    Paginator V2 + filtrar JSON por nombre también

--- src/app/pages/cards/cards.component.ts
+++ src/app/pages/cards/cards.component.ts
@@ -5,152 +5,75 @@ import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-cards',
-  templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
+	selector: 'app-cards',
+	templateUrl: './cards.component.html',
+	styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  totalCards: number = 100; // Total de cartas disponibles
-  pageSizeOptions = [10, 25, 50, 100];
-  cardsPerPage: number = this.pageSizeOptions[0]; // Número de cartas por página
-  currentPageIndex: number = 1; // Índice de la página actual
-  queryParamsSubscription: Subscription | undefined; // Suscripción al cambio de parámetros de consulta
+	@ViewChild(MatPaginator) paginator!: MatPaginator;
+	totalCards: number = 100; // Total de cartas disponibles
+	pageSizeOptions = [10, 25, 50, 100];
+	cardsPerPage: number = this.pageSizeOptions[0]; // Número de cartas por página
+	currentPageIndex: number = 1; // Índice de la página actual
+	queryParamsSubscription: Subscription | undefined; // Suscripción al cambio de parámetros de consulta
 
-  cards: any[] = [];
-  displayedCards: any[] = []; // Cartas a mostrar en la página actual
-  errorMessage: string | undefined;
-  searchTerm: any;
+	cards: any[] = [];
+	displayedCards: any[] = []; // Cartas a mostrar en la página actual
+	errorMessage: string | undefined;
+	searchTerm: any;
 
-  constructor(
-    private route: ActivatedRoute,
-    private apiService: ApiService
-  ) { }
+	constructor(
+		private route: ActivatedRoute,
+		private apiService: ApiService
+	) { }
 
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.searchTerm = params['name'];
-      const nPage = params['nPage'];
+	ngOnInit(): void {
+		this.route.queryParams.subscribe(params => {
+			this.searchTerm = params['name'];
+			const nPage = params['nPage'];
 
-      this.apiService.searchCards({ "name": this.searchTerm }, nPage)
-        .subscribe(
-          (response) => {
-            console.log('Resultados de búsqueda:', response);
-            if (response.cards.length > 0) {
-              this.cards = response.cards.filter((card: any) => card.imageUrl);
-              this.totalCards = this.cards.length;
-              this.paginator.pageIndex = nPage - 1;
+			this.apiService.searchCards({ "name": this.searchTerm }, nPage)
+				.subscribe(
+					(response) => {
+						console.log('Resultados de búsqueda:', response);
+						if (response.cards.length > 0) {
+							this.cards = response.cards.filter((card: any) => card.imageUrl);
+							this.cards = response.cards.filter((card: any, index: number, self: any[]) =>
+														index === self.findIndex((c: any) => c.name === card.name && c.imageUrl)
+														);
 
-              // Actualizar las cartas a mostrar en la página actual
-              this.updateDisplayedCards();
-            } else {
-              this.errorMessage = 'No hay resultados para esta búsqueda. Por favor, revisa que esté todo bien y prueba de nuevo.';
-              this.cards = [];
-              this.totalCards = 0;
-            }
-          },
-          (error) => {
-            console.error('Error al obtener las cartas:', error);
-            this.errorMessage = 'No se pudo obtener las cartas. Por favor, inténtalo de nuevo más tarde.';
-            this.cards = [];
-            this.totalCards = 0;
-          }
-        );
-    });
-  }
+							this.totalCards = this.cards.length;
+							this.paginator.pageIndex = nPage - 1;
 
-  onPageChange(event: PageEvent) {
-    this.currentPageIndex = event.pageIndex + 1;
+							// Actualizar las cartas a mostrar en la página actual
+							this.updateDisplayedCards();
+						} else {
+							this.errorMessage = 'No hay resultados para esta búsqueda. Por favor, revisa que esté todo bien y prueba de nuevo.';
+							this.cards = [];
+							this.totalCards = 0;
+						}
+					},
+					(error) => {
+						console.error('Error al obtener las cartas:', error);
+						this.errorMessage = 'No se pudo obtener las cartas. Por favor, inténtalo de nuevo más tarde.';
+						this.cards = [];
+						this.totalCards = 0;
+					}
+				);
+		});
+	}
 
-    // Actualizar las cartas a mostrar en la página actual
-    this.updateDisplayedCards();
-  }
+	onPageChange(event: PageEvent) {
+		this.currentPageIndex = event.pageIndex + 1;
+		this.cardsPerPage = event.pageSize;
 
-  updateDisplayedCards() {
-    const startIndex = (this.currentPageIndex - 1) * this.cardsPerPage;
-    const endIndex = startIndex + this.cardsPerPage;
-    this.displayedCards = this.cards.slice(startIndex, endIndex);
-  }
+		// Actualizar las cartas a mostrar en la página actual
+		this.updateDisplayedCards();
+	}
+
+	updateDisplayedCards() {
+		const startIndex = (this.currentPageIndex - 1) * this.cardsPerPage;
+		const endIndex = startIndex + this.cardsPerPage;
+		this.displayedCards = this.cards.slice(startIndex, endIndex);
+	}
 }
-
-
-
-// onPageChange() {
-// 	const currentPage = this.currentPageIndex + 1;
-// 	this.apiService.searchCards({ "name": this.searchTerm }, currentPage)
-// 		.subscribe(
-// 			(response) => {
-// 				console.log('Resultados de búsqueda:', response);
-// 				if (response.cards.length > 0) {
-// 					this.cards = response.cards;
-// 				} else {
-// 					this.errorMessage = 'No hay resultados para esta búsqueda. Por favor, revisa que esté todo bien y prueba de nuevo.';
-// 					this.cards = [];
-// 				}
-// 			},
-// 			(error) => {
-// 				console.error('Error al obtener las cartas:', error);
-// 				this.errorMessage = 'No se pudo obtener las cartas. Por favor, inténtalo de nuevo más tarde.';
-// 				this.cards = [];
-// 			}
-// 		);
-// }
-
-
-
-
-
-
-	// ngOnInit() {
-	// 	this.route.queryParams.subscribe(params => {
-	// 	  const searchTerm = params['name']; // Obtén el parámetro de búsqueda "name"
-
-	// 	  // Utiliza el servicio ApiService para realizar la solicitud de datos
-	// 	  this.apiService.getCards(searchTerm).subscribe(
-	// 		(response) => {
-	// 		  console.log('Resultados de búsqueda:', response);
-	// 		  // Realiza las acciones necesarias para mostrar los resultados en la página "cards"
-	// 		},
-	// 		(error) => {
-	// 			console.error('Error al obtener las cartas:', error);
-	// 			this.errorMessage = 'No se pudo obtener las cartas. Por favor, inténtalo de nuevo más tarde.';
-	// 			return of([]); // Devuelve un observable vacío para que el flujo continúe sin interrupciones
-	// 		}
-
-
-	// 	  );
-	// 	});
-	//   }
-
-
-
-
-
-
-
-	// ngOnInit(): void {
-	// 	this.route.queryParams.subscribe(params => {
-	// 		const name = params['name']; // Obtén el parámetro de búsqueda "name"
-
-	// 		// Realiza la solicitud de datos utilizando el parámetro de búsqueda
-	// 		this.searchCards({ "name": name }, this.nPage);
-	// 	});
-
-	// 	this.cardSrv.getCards("")
-	// 		.pipe(
-	// 			// tap((card: Card[]) => {
-	// 			// 	// Manejar los datos aqui
-	// 			//   card.forEach(pub => {
-	// 			// 	pub.user_name = "Undefined";
-	// 			//   });
-	// 			//   this.cards = card;
-	// 			// }),
-	// 			catchError(error => {
-	// 				// Manejar el error aquí
-	// 				console.error('Error al obtener las publicaciones:', error);
-	// 				this.errorMessage = 'No se pudo obtener las publicaciones. Por favor, inténtalo de nuevo más tarde.';
-	// 				return of([]); // Devuelve un observable vacío para que el flujo continúe sin interrupciones
-	// 			})
-	// 		)
-	// 		.subscribe();
-	// }
